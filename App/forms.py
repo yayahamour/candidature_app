a22ad89d5fc0662de4824c4371887cff5a51afc5
ayoubhaddou <ayoubhaddou1@gmail.com>
2022-02-07 11:58:49 +0100

    SelectField for Status

--- App/forms.py
+++ App/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField,EmailField,SubmitField,StringField, BooleanField
 from wtforms.validators import Length,DataRequired,Email,EqualTo,ValidationError
-from wtforms.fields import DateField
+from wtforms.fields import DateField, SelectField
 from .models import Users
 
 class Login(FlaskForm):
@@ -35,7 +35,7 @@ class ModifyCandidacy(FlaskForm):
     contact_full_name = StringField(label='contact_full_name', validators=[DataRequired()])
     contact_email = StringField(label='contact_email', validators=[DataRequired()])
     contact_mobilephone = StringField(label='contact_mobilephone')
-    status = StringField(label='Status', validators=[DataRequired()])
+    status = SelectField(label='Status', validators=[DataRequired()], choices=["En cours", "Accepté", "Refusé"])
     relance = BooleanField('A été relancé ? ')
     modif_date = DateField('Date', format='%Y-%m-%d')
     submit = SubmitField(label="Valider")
