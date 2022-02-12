17c468fee74ab0b263d5448a1c48a2d9ba2cee57
ayoubhaddou <ayoubhaddou1@gmail.com>
2022-02-12 23:02:24 +0100

    Test ajout supression offre et candidature fait

--- Test/Test_selenium_func.py
+++ Test/Test_selenium_func.py
@@ -7,17 +7,19 @@ driver = webdriver.Firefox(executable_path='./Driver/geckodriver')
 
 class Selenium_test:
     
-    def login_test(user, password) :
+    def clic_connexion(user,password):
+        driver.find_element_by_link_text('Connexion').click()
+        sleep(1)
         # Insert user and password in the login form 
         driver.find_element_by_id('email').send_keys(user)
         driver.find_element_by_id('password').send_keys(password)
         driver.find_element_by_name('submit').click()
-        
+
         # Assert h1 field is correct in this page
-        # Try + assert ou l'un ou l'autre uniquement ? 
         try:
             h1 = driver.find_element_by_tag_name('h1')
             assert h1.text == "CANDIDATURES"
+            print('Connexion success')
         except AttributeError:
             print('ERROR ----- h1 not found')
         # Assert Flash login success 
@@ -26,3 +28,102 @@ class Selenium_test:
         print('------------------test login user Done----------------------')
         
         
+
+    def add_candidacy_test(plateform_name, contact_test):
+        # To click to the button add candidacy
+        driver.find_element_by_link_text('Ajouter candidature').click()
+        
+        # Insert the candidacy in the form 
+        driver.find_element_by_id('plateforme').send_keys(plateform_name) 
+        driver.find_element_by_id('entreprise').send_keys('Ikea') 
+        driver.find_element_by_id('lieu').send_keys("Lille") 
+
+        # Click on the button add candidacy
+        driver.find_element_by_name('submit').click()
+
+        # Assert flash element added success 
+        try:
+                
+            flash_succes = driver.find_element_by_class_name('alert').text
+            assert 'Nouvelle Candidature ajouté' in flash_succes 
+            # Assert element is on dashboard's following place => Front test
+            td_entreprise_xpath = "//tbody/tr[2]/td[1]"
+            td_entreprise = driver.find_element(By.XPATH, td_entreprise_xpath)
+            assert td_entreprise.text in plateform_name
+            print('------------------test add candidacy Done with success----------------------')
+
+        except:
+            print(' -------ERROR!  Add candidacy test error : Plateform name not at the good place --------ERROR!')
+
+
+    def delete_candidacy_test() :
+        # Click on the third button delete item 
+        delete_xpath = '//tbody/tr/td[12]/a[2]'
+        try:
+            driver.find_element(By.XPATH, delete_xpath).click()
+            flash_succes = driver.find_element_by_class_name('alert').text
+            assert "Candidature supprimée avec succès" in flash_succes
+            print('------------------delete candidacy done----------------------')
+
+        except:
+            print('Cannot found the delete button --------ERROR!')
+
+        # need order dashboard by last added to assert 'word' not in [xpath row[1]] 
+        # Or need candidacy id on admin dashboard to assert deleted
+        # Or need to use Candidacy.query.filter_by(name=["element to check"]) methods ?
+
+        
+    def clic_offres():
+        driver.find_element_by_link_text('Offres').click()
+        try:
+            h1 = driver.find_element_by_tag_name('h1')
+            assert h1.text == "OFFRES D'ALTERNANCE"
+            print('------------------Offres page reached with success---------------------')
+        except AttributeError:
+            print('ERROR : Title not found --------ERROR! ')
+        
+        
+    def add_offer_test(entreprise_name, contact_test):
+        # To click to the button add candidacy
+        driver.find_element_by_link_text('Ajouter offre').click()
+        
+        # Insert the candidacy in the form 
+        driver.find_element_by_id('entreprise').send_keys(entreprise_name) 
+        driver.find_element_by_id('lieu').send_keys('Ikea') 
+        driver.find_element_by_id('contact_full_name').send_keys(contact_test) 
+
+        # Click on the button add candidacy
+        driver.find_element_by_name('submit').click()
+
+        # Assert flash element added success 
+        try:
+                
+            flash_succes = driver.find_element_by_class_name('alert').text
+            assert "Nouvelle offre d'emploi ajoutée" in flash_succes 
+            td_entreprise_xpath = "//tbody/tr[2]/td[2]"
+            td_entreprise = driver.find_element(By.XPATH, td_entreprise_xpath)
+            assert td_entreprise.text == entreprise_name
+            print('------------------test add offer Done----------------------')
+        except:
+            print('Error add offer test : Plateform name not found-------ERROR! ')
+
+
+
+    def delete_offer_test() :
+        # Click on the third button delete item 
+        
+        try:
+            delete_xpath = '//tbody/tr[2]/td[10]/a[3]'
+            driver.find_element(By.XPATH, delete_xpath).click()
+            flash_succes = driver.find_element_by_class_name('alert').text
+            assert "Offre d'emploi supprimée avec succès" in flash_succes
+            print('------------------test offer deleted with succées ---------')
+        except:
+            print('Cannot found the delete button   --------ERROR!')
+
+        # need order dashboard by last added to assert 'word' not in [xpath row[1]] 
+        # Or need candidacy id on admin dashboard to assert deleted
+        # Or need to use Candidacy.query.filter_by(name=["element to check"]) methods ?
+
+        print('------------------delete candidacy n°3 done----------------------')
+   
--- Test/Test_selenium_script.py
+++ Test/Test_selenium_script.py
@@ -4,7 +4,21 @@ from selenium.webdriver.common.by import By
 from time import sleep 
 
 
-url = 'https://candiapp.herokuapp.com/login'
+url = 'http://candidature-app1.herokuapp.com/profil'
 
 
 driver.get(url)
+
+Selenium_test.clic_connexion("yayahamour@gmail.com","1234")
+
+Selenium_test.add_candidacy_test('Linkedin','eric@gmail.com')
+
+Selenium_test.delete_candidacy_test()
+
+Selenium_test.clic_offres()
+
+Selenium_test.add_offer_test("Apple", "Eric")
+Selenium_test.delete_offer_test() 
+
+
+# driver.quit()
